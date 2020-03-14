ba8b0e922167b39877d32a9d67cbf0e5bf2dfbbf
ZhangHNan <a2857327284@163.com>
2020-03-14 20:31:18 +0800

    modified

--- src/main/java/wanzhi/gulu/community/service/QuestionService.java
+++ src/main/java/wanzhi/gulu/community/service/QuestionService.java
@@ -24,15 +24,15 @@ public class QuestionService {
     @Autowired
     PageUtils pageUtils;
 
-    @Value("${page.rows}")
-    private String rows;//设置每页展示数据行数
+    @Value("${page.index.rows}")
+    private String indexRows;//设置每页展示数据行数
 
-    @Value("${page.buttonCount}")
-    private String buttonCount;//设置每页展示页面按钮数。请设置为奇数，设置为偶数中间段还是奇数个，头和尾才是偶数个
+    @Value("${page.index.buttonCount}")
+    private String indexButtonCount;//设置每页展示页面按钮数。请设置为奇数，设置为偶数中间段还是奇数个，头和尾才是偶数个
 
     //查询帖子并作分页
     public PageDTO findPage(Integer currentPage){
         //传入要跳转的页面、每页显示数据条数、每页显示指定到某页的按钮数即可自动构建pageDTO对象并返回
-        return pageUtils.autoStructurePageDTO(currentPage, Integer.parseInt(rows),Integer.parseInt(buttonCount));
+        return pageUtils.autoStructurePageDTO(currentPage, Integer.parseInt(indexRows),Integer.parseInt(indexButtonCount));
     }
 }
