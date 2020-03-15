0a98aacc8a44aea657682cfa2ae7d28e55d12796
ZhangHNan <a2857327284@163.com>
2020-03-15 14:14:13 +0800

    modified

---
+++ src/main/java/wanzhi/gulu/community/controller/QuestionController.java
@@ -0,0 +1,14 @@
+package wanzhi.gulu.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class QuestionController {
+
+    @GetMapping("/question/{id}")
+    public String question(@PathVariable("id") Integer id){
+        return "question";
+    }
+}
