f01906566ae00eeb695610e92e80e8a29b243e78
ZhangHNan <a2857327284@163.com>
2020-03-19 01:51:22 +0800

    comment

--- src/main/resources/static/js/comment.js
+++ src/main/resources/static/js/comment.js
@@ -14,7 +14,15 @@ function post(){
             if(response.code == 200){
                 $("#comment_area").hide();
             }else{
-                alert(response.message);
+                if(response.code == 3005){
+                    var isAccepted = confirm(response.message);
+                    if(isAccepted){
+                        window.open("https://github.com/login/oauth/authorize?client_id=c14b8a6a1a6b4e02b9a1&redirect_uri=http://localhost:8887/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable","true");//设置closable，当登录成功跳转到首页时判断，如果有closable等于true则删除closable并关闭浏览器
+                    }
+                }else{
+                    alert(response.message);
+                }
             }
         },
         dataType:"json"
