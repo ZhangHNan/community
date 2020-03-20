aa70088b0b43a29c372376d85e073f2b162c78e4
ZhangHNan <a2857327284@163.com>
2020-03-20 21:22:41 +0800

    Update

---
+++ src/main/java/wanzhi/gulu/community/enums/NotificationEnum.java
@@ -0,0 +1,22 @@
+package wanzhi.gulu.community.enums;
+
+public enum NotificationEnum {
+    REPLY_QUESTION(1,"回复了问题"),
+    REPLY_COMMENT(2,"回复了评论")
+    ;
+    private int status;
+    private String name;
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    NotificationEnum(int status, String name) {
+        this.status = status;
+        this.name = name;
+    }
+}
